d5d995bcd57e53befb36553a8394783ffaa5098d
Anjacodes <anja_schmidt7@posteo.de>
2022-05-03 11:37:28 -0500

    Create Greetings Controller Generating Random Greetings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :v1 do
+    get 'greetings', to: 'greetings#hello'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
